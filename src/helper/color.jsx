d5ea5f9a737d656d4dbcedc6a7b9e733f24a77c2
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-10-11 19:57:02 +0200

    Add function of colors in helper page

---
+++ src/helper/color.jsx
@@ -0,0 +1,6 @@
+
+export default function Colors(type) {
+if(type === 'simple') return 'bg-[#E17654]'
+if(type === 'rugged') return 'bg-[#115E59]'
+if(type === 'luxury') return 'bg-[#161616]'
+}
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import "../server"
+import Colors from '../helper/color'
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
@@ -10,19 +11,19 @@ export default function Vans() {
         
     }, [])
     const vansElement = vans.map(values=>(
-        <div key={values.id} className='py-12 px-10'>
+        <div key={values.id} className='py-12 px-20'>
             <img className='w-80 h-80 rounded-lg' src={values.imageUrl} alt="" />
             <div>
                 <h3 className='text-2xl font-bold py-4'>{values.name}</h3>
-                <p>${values.price}<span>/day</span></p>
+                <p className='py-6'>${values.price}<span>/day</span></p>
             </div>
-            <i className={`py-4 px-10 ${colors(values.type)}`}>{values.type}</i>
+            <i className={`py-3 px-8 rounded-lg text-white ${Colors(values.type)}`}>{values.type}</i>
         </div>
     ))
   return (
     <div>
     <h1 className='text-4xl font-bold text-center py-4'>Explore our van options</h1>
-    <div className='grid grid-cols-2 gap-8'>
+    <div className='grid grid-cols-2 justify-around'>
      {vansElement}
     </div>
     </div>
